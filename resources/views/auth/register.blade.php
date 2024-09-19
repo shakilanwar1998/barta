e85e5d75af05b473758aff8ba5339146417f1a12
Shakil Anwar <shakilanwar1998@gmail.com>
2024-09-19 19:39:11 +0600

    Readme

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,23 +38,23 @@
                 </div>
 
                 <!-- Username -->
-                <div>
-                    <label
-                        for="username"
-                        class="block text-sm font-medium leading-6 text-gray-900"
-                    >Username</label
-                    >
-                    <div class="mt-2">
-                        <input
-                            id="username"
-                            name="email"
-                            type="text"
-                            autocomplete="username"
-                            placeholder="alparslan1029"
-                            required
-                            class="block w-full rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" />
-                    </div>
-                </div>
+{{--                <div>--}}
+{{--                    <label--}}
+{{--                        for="username"--}}
+{{--                        class="block text-sm font-medium leading-6 text-gray-900"--}}
+{{--                    >Username</label--}}
+{{--                    >--}}
+{{--                    <div class="mt-2">--}}
+{{--                        <input--}}
+{{--                            id="username"--}}
+{{--                            name="username"--}}
+{{--                            type="text"--}}
+{{--                            autocomplete="username"--}}
+{{--                            placeholder="alparslan1029"--}}
+{{--                            required--}}
+{{--                            class="block w-full rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" />--}}
+{{--                    </div>--}}
+{{--                </div>--}}
 
                 <!-- Email -->
                 <div>
